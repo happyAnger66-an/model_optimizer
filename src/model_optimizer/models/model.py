614d7e3e8050e4ecc926e58ecf5f0d21cb1259de
zhangxiaoan <happyAnger66@163.com>
2026-01-19 20:46:40 +0800

    feat: refactor quantize codes.

--- src/model_optimizer/models/model.py
+++ src/model_optimizer/models/model.py
@@ -1,5 +1,8 @@
+from functools import partial
 import torch.nn as nn
 
+import modelopt.torch.opt as mto
+import modelopt.torch.quantization as mtq
 
 
 class Model:
@@ -13,6 +16,36 @@ class Model:
     def export_onnx(self, *args, **kwargs):
         raise NotImplementedError
 
+    def quantize_start(self, quant_cfg, quant_mode, calib_data, export_dir):
+        pass
+
+    def quantize_end(self):
+        pass
+
     def quantize(self, quant_cfg, calib_data, calib_method):
-        from model_optimizer.quantization.quantization_utils import quantize_model
-        quantize_model(self, quant_cfg, calib_data, calib_method)
+        self.quantize_start(quant_cfg, calib_data, calib_method)
+
+        calibrate_loop = self.get_model_calibrate_loop(calib_data)
+        mto.quantize(self.model, quant_cfg,
+                     forward_loop=calibrate_loop)
+        print(f'quantize summary')
+        mto.print_quant_summary(self.model)
+
+        self.quantize_end()
+
+        # from model_optimizer.quantization.quantization_utils import quantize_model
+        # quantize_model(self, quant_cfg, calib_data, calib_method)
+
+    def get_model_calibrate_loop(self, calib_data):
+        datasets = self.get_calibrate_dataset(calib_data)
+
+        def calibrate_loop(model):
+            for idx, data in enumerate(datasets):
+                if data is None:
+                    print('calibrate data is None')
+                    continue
+
+                print(f'calibration idx {idx} data:{data}')
+                model(data)
+
+        return partial(calibrate_loop, self.model)
