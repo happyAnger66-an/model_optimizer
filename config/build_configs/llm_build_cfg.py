2af25b4d82d6d0b7e9ef1d69c647c9276a16c0f9
zhangxiaoan <happyAnger66@163.com>
2026-02-12 20:25:42 +0800

    feat: add llm build cfg.

---
+++ config/build_configs/llm_build_cfg.py
@@ -0,0 +1,19 @@
+build_cfg = {
+    "precision": "bf16",
+    "workspace_mb": 8192,
+    "min_shapes": {
+        "inputs_embeds": (1, 968, 2048),
+        "attention_mask": (1, 1, 968, 968),
+        "position_ids": (1, 968),
+    },
+    "opt_shapes": {
+        "inputs_embeds": (1, 968, 2048),
+        "attention_mask": (1, 1, 968, 968),
+        "position_ids": (1, 968),
+    },
+    "max_shapes": {
+        "inputs_embeds": (1, 968, 2048),
+        "attention_mask": (1, 1, 968, 968),
+        "position_ids": (1, 968),
+    }
+}
