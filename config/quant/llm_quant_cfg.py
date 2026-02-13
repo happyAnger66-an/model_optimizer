bd42cf25387b871693dddc10c953cc0e6f0e65e3
zhangxiaoan <happyAnger66@163.com>
2026-02-13 17:46:20 +0800

    fix: fix llm quant cfg.

--- config/quant/llm_quant_cfg.py
+++ config/quant/llm_quant_cfg.py
@@ -1,8 +1,8 @@
 from modelopt.torch.quantization import INT8_DEFAULT_CFG, INT8_SMOOTHQUANT_CFG, FP8_DEFAULT_CFG
 QUANT_CFG = INT8_DEFAULT_CFG
 
-QUANT_CFG["algorithm"] = "max"
-QUANT_CFG["mode"] = "int8"
+#QUANT_CFG["algorithm"] = "max"
+#QUANT_CFG["mode"] = "int8"
 QUANT_CFG["quant_cfg"]["input_layernorm"] = {"enable": False}
 QUANT_CFG["quant_cfg"]["post_attention_layernorm"] = {"enable": False}
 QUANT_CFG["quant_cfg"]["norm"] = {"enable": False}
