4388115b50d350ad9e7b4d5d8e0b2ff034f4d2e9
happyAnger66-an <happyAnger66-an@163.com>
2026-02-14 20:50:53 +0800

    feat: llm use fp8.

--- config/quant/llm_quant_cfg.py
+++ config/quant/llm_quant_cfg.py
@@ -1,5 +1,5 @@
 from modelopt.torch.quantization import INT8_DEFAULT_CFG, INT8_SMOOTHQUANT_CFG, FP8_DEFAULT_CFG
-QUANT_CFG = INT8_DEFAULT_CFG
+QUANT_CFG = FP8_DEFAULT_CFG
 
 #QUANT_CFG["algorithm"] = "max"
 #QUANT_CFG["mode"] = "int8"
