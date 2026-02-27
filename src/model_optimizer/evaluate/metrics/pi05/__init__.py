c05be4aba7019d77209d09fb8b6577ca034138e5
zhangxiaoan <happyAnger66@163.com>
2026-02-27 17:27:49 +0800

    fix: fix

--- src/model_optimizer/evaluate/metrics/pi05/__init__.py
+++ src/model_optimizer/evaluate/metrics/pi05/__init__.py
@@ -8,7 +8,8 @@ class Pi05Metric(Metric):
     def compare(self, other: Metric):
         other_result = other.get_result()
         self_result = self.get_result()
-        compare_predictions(self_result, other_result, key1="Origin", key2="Quantized")
+        for origin_result, quantized_result in zip(self_result, other_result):
+            compare_predictions(origin_result, quantized_result, key1="Origin", key2="Quantized")
 
     def print(self):
         pass
