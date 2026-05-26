87014f76d9808cc78ee794a88d1cb060d275b57d
happyAnger66-an <happyAnger66@163.com>
2026-05-26 21:31:16 +0800

    fix: fix compile error

--- cpp/kernels/fmha/cuteDslFMHAD256Runner.cpp
+++ cpp/kernels/fmha/cuteDslFMHAD256Runner.cpp
@@ -6,6 +6,10 @@
 
 #include <mutex>
 
+#ifdef CUTE_DSL_FMHA_ENABLED
+#include "cutedsl_all.h"
+#endif
+
 namespace model_opt
 {
 
@@ -21,8 +25,6 @@ bool CuteDslFMHAD256Runner::canImplement(int32_t headDim, int32_t smVersion)
 }
 
 #ifdef CUTE_DSL_FMHA_ENABLED
-
-#include "cutedsl_all.h"
 
 static fmha_d256_homo_bf16_Kernel_Module_t sModuleBf16{};
 static fmha_d256_homo_fp16_Kernel_Module_t sModuleFp16{};
@@ -56,12 +58,18 @@ void CuteDslFMHAD256Runner::unloadKernelModules()
 static int32_t runVariant(
     FmhaD256Params const& p, cudaStream_t stream, bool useFp16)
 {
+    int32_t const windowSizeLeft{0};
+    float const scaleQ{1.0F};
+    float const scaleK{1.0F};
+    float const scaleV{1.0F};
+    float const invScaleO{1.0F};
+
     if (useFp16)
     {
-        fmha_d256_homo_fp16_Tensor_q_t q{};
-        fmha_d256_homo_fp16_Tensor_kv_t kv{};
-        fmha_d256_homo_fp16_Tensor_o_t o{};
-        fmha_d256_homo_fp16_Tensor_cu_kv_t cu{};
+        fmha_d256_homo_fp16_Tensor_q_tensor_t q{};
+        fmha_d256_homo_fp16_Tensor_kv_cache_t kv{};
+        fmha_d256_homo_fp16_Tensor_o_tensor_t o{};
+        fmha_d256_homo_fp16_Tensor_cum_seqlen_k_t cu{};
 
         q.data = const_cast<void*>(p.qPtr);
         kv.data = const_cast<void*>(p.kvPtr);
@@ -87,13 +95,14 @@ static int32_t runVariant(
         cu.dynamic_shapes[0] = p.batchSize + 1;
 
         return cute_dsl_fmha_d256_homo_fp16_wrapper(
-            &sModuleFp16, &q, &kv, &o, &cu, stream);
+            &sModuleFp16, &q, &kv, &o, &cu, windowSizeLeft, scaleQ, scaleK,
+            scaleV, invScaleO, stream);
     }
 
-    fmha_d256_homo_bf16_Tensor_q_t q{};
-    fmha_d256_homo_bf16_Tensor_kv_t kv{};
-    fmha_d256_homo_bf16_Tensor_o_t o{};
-    fmha_d256_homo_bf16_Tensor_cu_kv_t cu{};
+    fmha_d256_homo_bf16_Tensor_q_tensor_t q{};
+    fmha_d256_homo_bf16_Tensor_kv_cache_t kv{};
+    fmha_d256_homo_bf16_Tensor_o_tensor_t o{};
+    fmha_d256_homo_bf16_Tensor_cum_seqlen_k_t cu{};
 
     q.data = const_cast<void*>(p.qPtr);
     kv.data = const_cast<void*>(p.kvPtr);
@@ -119,7 +128,8 @@ static int32_t runVariant(
     cu.dynamic_shapes[0] = p.batchSize + 1;
 
     return cute_dsl_fmha_d256_homo_bf16_wrapper(
-        &sModuleBf16, &q, &kv, &o, &cu, stream);
+        &sModuleBf16, &q, &kv, &o, &cu, windowSizeLeft, scaleQ, scaleK,
+        scaleV, invScaleO, stream);
 }
 
 int32_t CuteDslFMHAD256Runner::run(FmhaD256Params const& params, cudaStream_t stream)
